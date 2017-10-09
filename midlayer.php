0da8746a2ea60e61ec8cc7ee6924680270da738e
Todd Austin <austin.todd.j@gmail.com>
2017-10-09 14:51:47 -0500

    Form validation updates

--- midlayer.php
+++ midlayer.php
@@ -1,3 +1,16 @@
 <?php
 
-// Silence is golden.
+$url = 'https://full-jck.cs23.force.com/CSRWebService/services/apexrest/CSRWS';
+$data = $_POST;
+
+$options = [
+    'http' => [
+        'header'  => "Content-type: application/x-www-form-urlencoded",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    ]
+];
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+var_dump($result);
